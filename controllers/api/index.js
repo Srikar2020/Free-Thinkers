78fc6a7318e6f5d8b1ba975fb693f7864fcea7b7
andytang02 <andytang02@gmail.com>
2020-11-04 11:02:04 -0800

    user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-//const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes.js');
 const categoryRoutes = require('./category-routes.js');
 //const commentRoutes = require('./comment-routes');
 
-//router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 //router.use('/comments', commentRoutes);
 
